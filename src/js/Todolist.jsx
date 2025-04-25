305235a31b932aa1c86af9180c82cb854d78eac7
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-25 19:32:44 +0000

    primeros cambios

---
+++ src/js/Todolist.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import React from "react";
+ 
+
+export const Todolist = () => {
+    return (
+       <>
+        <h1> <strong>TAREAS DE SEGUNDO</strong> </h1>
+        <div className="tareas">
+        
+        </div>
+        </>
+    )
+}
