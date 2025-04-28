b8b8d19a9e6f4390bc9c30e0b6908b0b329bac12
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-28 19:06:13 +0000

    primeros cambios

--- src/js/Todolist.jsx
+++ src/js/Todolist.jsx
@@ -1,14 +1,48 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import React from "react";
- 
+
 
 export const Todolist = () => {
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+
+    const deletetask = (taskIndex) => {
+        const newTasks = tasks.filter((_, index) => index !== taskIndex)
+        setTasks(newTasks);
+    };
+
+
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && inputValue.trim() !== '') {
+            setTasks([...tasks, inputValue]);
+            setInputValue('');
+        }
+    };
     return (
-       <>
-        <h1> <strong>TAREAS DE SEGUNDO</strong> </h1>
-        <div className="tareas">
-        
-        </div>
+        <>
+           
+            <div className="tareas">
+            <h1> <strong>TAREAS DE SEGUNDO</strong> </h1>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
+                {
+                    tasks.map((task, index) => (
+                        <li key={index} className="task-item">
+                            {task}
+                            <button onClick={() => deletetask(index)}>X</button>
+                        </li>
+                    )
+
+                    )
+                }
+
+            </div>
         </>
     )
 }
